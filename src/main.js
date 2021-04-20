b972f1055cf03d37b9427ca6caf53bf67c48c399
Yoon Hee Cho <yoonheecho1123@gmail.com>
2021-04-20 11:33:22 -0400

    deleted comments

--- src/main.js
+++ src/main.js
@@ -36,14 +36,7 @@ const game = new GameBuilder()
  gameFinishBanner.setOnClick(() => {
      game.start()
  });
-// gameFinishBanner.setOnclick(game.start());
-// 이거는 왜 DOM에 오류가 생기지? 
-// 왜 바인딩은 에로우 펑션을 이용해면 this가 전달되는거지? -> 자동으로 바인딩이 됨 
-//(e)=>{game.start(e)} // game.start
-// game.start안에 this.에 접근하기 때문에 바인딩을 수동적으로 해주거나 arrow function을 써야 해요 
 
-
-//innerHTML innerText의 차이점
 
 
 
